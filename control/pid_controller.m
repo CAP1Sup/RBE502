9d33440bcd68efe8ae7b9fd59aa5eb7ae154d0ec
CAP1Sup <xmanpiper@gmail.com>
2024-11-03 14:33:17 -0500

    Improved PD controller performance, minor calculation changes
    Moved to separate position and attitude gains

--- control/pid_controller.m
+++ control/pid_controller.m
@@ -20,31 +20,34 @@ function [F, M, trpy, drpy] = pid_controller(qd, t, qn, params)
         icnt = 5; % Force the first iteration to update the attitude
     end
 
-    % =================== Your code starts here ===================
-    % yaw is psi
-    % roll is phi
-    % pitch is theta
+    % Coefficients for the position PD controller
+    posP = 15;
+    posD = 5;
+    % In form: [pX, dX; pY, dY; pZ, dZ]
+    kp = [posP, posD;
+          posP, posD;
+          100, 20];
 
-    % Coefficents for the moment PD controller
+    % Coefficents for the attitude PD controller
     % In form: [pRoll, dRoll; pPitch, dPitch; pYaw, dYaw]
-    rotP = 1;
-    rotD = 1.5;
-    km = [rotP, rotD;
-          rotP, rotD;
-          1, 2];
-
-    % Coefficients for the position PD controller (only in z)
-    kz = [5, 2];
+    attP = 3000;
+    attD = attP / 10;
+    ka = [attP, attD;
+          attP, attD;
+          attP, attD];
 
     % Readability
-    phi = qd{qn}.euler(1);
-    theta = qd{qn}.euler(2);
-    %psi = qd{qn}.euler(3); Not used
+    phi = qd{qn}.euler(1); % Roll
+    theta = qd{qn}.euler(2); % Pitch
+    %psi = qd{qn}.euler(3); Yaw, not used
 
-    kmP = km(:, 1);
-    kmD = km(:, 2);
-    kzP = kz(1);
-    kzD = kz(2);
+    kpP = kp(:, 1);
+    kpD = kp(:, 2);
+    kaP = ka(:, 1);
+    kaD = ka(:, 2);
+
+    % Desired accelerations (general form)
+    acc_des = qd{qn}.acc_des + kpD .* (qd{qn}.vel_des - qd{qn}.vel) + kpP .* (qd{qn}.pos_des - qd{qn}.pos);
 
     % Position Controller
     % Uses a 3D trajectory controller to determine the desired angles and angular velocities
@@ -53,16 +56,13 @@ function [F, M, trpy, drpy] = pid_controller(qd, t, qn, params)
     if (icnt == 5)
         icnt = 0;
 
-        % Desired accelerations (general form)
-        acc_des = qd{qn}.acc_des + kmD .* (qd{qn}.vel_des - qd{qn}.vel) + kmP .* (qd{qn}.pos_des - qd{qn}.pos);
-
         % Desired angles
         phi_des = 1 / params.grav * (acc_des(1) * sin(qd{qn}.yaw_des) - acc_des(2) * cos(qd{qn}.yaw_des));
         theta_des = 1 / params.grav * (acc_des(1) * cos(qd{qn}.yaw_des) + acc_des(2) * sin(qd{qn}.yaw_des));
         psi_des = qd{qn}.yaw_des;
 
         % Bound the angles
-        max_angle = 80 * pi / 180;
+        max_angle = 60 * pi / 180;
         phi_des = max(min(phi_des, max_angle), -max_angle);
         theta_des = max(min(theta_des, max_angle), -max_angle);
 
@@ -78,18 +78,12 @@ function [F, M, trpy, drpy] = pid_controller(qd, t, qn, params)
     % Assemble angular velocities into a useful form
     desired_angular_vels = [p_des; q_des; r_des];
 
-    % Calculations
-    angular_vels = [cos(theta), 0, -cos(phi) * sin(theta);
-                    0, 1, sin(phi);
-                    sin(theta), 0, cos(phi) * cos(theta)] * qd{qn}.omega;
-
     % Attitute Control
-    M = params.I * (kmP .* (desired_angles - qd{qn}.euler) + kmD .* (desired_angular_vels - angular_vels));
+    M = params.I * (kaP .* (desired_angles - qd{qn}.euler) + kaD .* (desired_angular_vels - qd{qn}.omega));
 
     % Thrust Control
-    F = params.mass * params.grav - params.mass * (kzD * (qd{qn}.vel(3) - qd{qn}.vel_des(3)) + kzP * (qd{qn}.pos(3) - qd{qn}.pos_des(3)));
-
-    % =================== Your code ends here ===================
+    % Vector compensationed for the angle of the quadrotor
+    F = params.mass * params.grav + params.mass * acc_des(3) * cos(phi) * cos(theta);
 
     % Output trpy and drpy as in hardware
     % Desired thrust, roll, pitch, and yaw
